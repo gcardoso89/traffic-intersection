03a8ff0551edda85b41d3f0de890db2c42a94c15
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2017-03-09 10:37:52 +0000

    Added webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require( 'path' );
+var webpack = require( "webpack" );
+
+module.exports = {
+	entry: [ './public/js/main.js' ],
+	output: {
+		path: path.join( __dirname, '/public/js/' ),
+		filename: 'app.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: path.join( __dirname, '/public/js/' ),
+				loader: 'babel-loader',
+				query: {
+					presets: [ 'es2015', 'stage-2' ]
+				}
+			}
+		]
+	},
+	plugins: [ new webpack.optimize.UglifyJsPlugin( { minimize: true } ) ],
+	devtool: 'source-map'
+};
